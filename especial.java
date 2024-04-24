ba5aa9aa40660f28ab31e454509095124c8858c9
MauroMFP <mauricioferreiraprederigo@gmail.com>
2024-04-23 23:16:06 -0300

    adição cilindro, cone, tronco do cone, tronco piramide e carcaça da piramide

--- especial.java
+++ especial.java
@@ -4,6 +4,12 @@ public class especial extends calculadora{
     double a;
     double b;
     double c;
+    double r;
+    double h;
+    double g;
+    double L;
+    double l;
+    double R;
 
     public void cubo(){
         double volumecubo = (int) Math.pow(this.a, 3);
@@ -16,6 +22,24 @@ public class especial extends calculadora{
         double diametroparal = Math.sqrt(Math.pow(this.a,2)+Math.pow(this.b,2)+Math.pow(this.c,2));
     }
     public void cilindro(){
+        double areabasecilindro = Math.PI * Math.pow(r, 2);
+        double arealateralcilindro = 2* Math.PI *  r * h;
+        double volumeCilindro = Math.PI * Math.pow(r, 2) * h;
+    }
+    public void cone(){
+        double arealateralcone = Math.PI * r * g;
+        double areabasecone = Math.PI * Math.pow( r,2);
+        double volumecone = (Math.PI * Math.pow( r,2) * h)/ 3;
+    }
+    public void troncopiramide(){
+        double SB = Math.pow(L,2);
+        double Sb = Math.pow(l,2);
+        double volumetroncocone = h/3 * (SB * Math.sqrt(SB*Sb)+Sb);
+    }
+    public void troncocone(){
+        double volumecone = Math.PI * h * (Math.pow( r,2) + R*r* + Math.pow(R,2))/ 3;
+    }
+    public void piramide(){
         
     }
 
